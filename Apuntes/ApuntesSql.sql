78486f1960d6c654866edf03c07db7d21b4eae88
davidgldv <davidgldv@gmail.com>
2018-05-12 17:08:44 -0400

    Apuntes

--- Apuntes/ApuntesSql.sql
+++ Apuntes/ApuntesSql.sql
@@ -16,25 +16,32 @@ SELECT  E.CodigoEmpleado, COUNT(C.CodigoCliente) as NºClientes FROM Empleados a
 SELECT CodigoEmpleadoRepVentas, COUNT(CodigoCliente) as NºClientes FROM Clientes GROUP BY CodigoEmpleadoRepVentas; /*Manera mas eficiente*/
 
 /*Sacar numero de clientes que no tienen asignado RepVentas*/
+
 SELECT COUNT(CodigoCliente) as NºClientes FROM Clientes GROUP BY CodigoEmpleadoRepVentas HAVING CodigoEmpleadoRepVentas IS NULL;  
 
 /*Sacar el primer pago y el ultimo de algún cliente*/
+
 SELECT MAX(FechaPago) ,MIN(FechaPago),CodigoCliente FROM Pagos GROUP BY CodigoCliente;
 
 /*Sacar el codigo cliente de aquellos clientes que hicieron pago en 2008*/
+
 SELECT CodigoCliente FROM Pagos WHERE YEAR(FechaPago)=2008;
 
 /*Sacar numero pedido, codigo cliente, fecha requerida y fecha entrega de los pedidos que no han sido integrados a tiempo*/
+
 SELECT P.CodigoPedido,P.CodigoCliente,P.FechaEsperada,P.FechaEntrega FROM  Pedidos as P  WHERE FechaEsperada > FechaEntrega;
 
 /*Sacar cuantos productos existen cada linea de pedido*/
 
 SELECT COUNT(CodigoProducto),CodigoPedido FROM DetallePedidos GROUP BY CodigoPedido;  
+
 /*Sacar listado de los 20 codigos de producto mas pedidos ordenados por cantidad pedida*/
+
 SELECT CodigoProducto ,Cantidad FROM DetallePedidos ORDER BY Cantidad DESC LIMIT 20;
 
 /*Sacar numero pedido, codigo clientes, fecha entrega, fecha requerida de los pedidos cuya fecha entrega ha sido al menos 2 dias antes de la fecha requerida*/
 
+SELECT P.CodigoPedido,P.CodigoCliente,P.FechaEsperada,P.FechaEntrega FROM  Pedidos as P  WHERE  FechaEntrega  < FechaEsperada ;
 
 /*Sacar la facturacion que ha tenido la empresa en toda la historia indicando la mas imposible suma(costeproducto*numero udsvendidas), el IVA y el total facturado(suma dos campos anteriores)*/
 
@@ -43,16 +50,16 @@ SELECT CodigoProducto ,Cantidad FROM DetallePedidos ORDER BY Cantidad DESC LIMIT
 
 
 /*Obtener listado del nombre de empleados con el nombre de sus jefes*/
-
+  
 
 /*Sacar listado de jefes y sacar empleados a su cargo ordenado por numero empleados subordinados*/
-
+SELECT CodigoJefe,CodigoEmpleado FROM Empleados ORDER BY 
 
 /*Obtener nombre de clientes a los que no se les ha entregado a tiempo un pedido*/
 
+SELECT DISTINCT C.NombreCliente FROM  Pedidos as P JOIN Clientes AS C ON P.CodigoCliente = C.CodigoCliente  WHERE FechaEsperada > FechaEntrega;
 
 /*Sacar em importe medio de los pedidos*/
-
-
+SELECT AVG(Cantidad*PrecioUnidad) FROM DetallePedidos GROUP BY CodigoPedido;
 /*Cual es el pedido mas caro del empleado que mas clientes tiene*/
 
